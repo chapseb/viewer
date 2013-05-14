b952fc4cbc206985d22ab16b0ba1f931920d8cda
Johan Cwiklinski <johan.cwiklinski@anaphore.eu>
2013-05-14 09:20:32 +0200

    Add external configuration path possibility

--- web/index.php
+++ web/index.php
@@ -13,7 +13,17 @@
 //Main application entry for production
 define('APP_DIR', realpath(__DIR__  . '/../app'));
 define('WEB_DIR', realpath(__DIR__  . '/../web'));
-define('APP_DEBUG', false);
-define('APP_CACHE', APP_DIR . '/cache/');
+
+//include local config path
+if ( file_exists(APP_DIR . '/config/config.inc.php') ) {
+    include_once APP_DIR . '/config/config.inc.php';
+}
+
+if ( !defined('APP_DEBUG') ) {
+    define('APP_DEBUG', false);
+}
+if ( !defined('APP_CACHE') ) {
+    define('APP_CACHE', APP_DIR . '/cache/');
+}
 
 require_once APP_DIR . '/main.php';
