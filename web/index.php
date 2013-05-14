f21561269ee35b77a4b5dfba0cde54be32aa6f12
Johan Cwiklinski <johan.cwiklinski@anaphore.eu>
2013-05-14 09:20:10 +0200

    PHPDoc

--- web/index.php
+++ web/index.php
@@ -1,4 +1,15 @@
 <?php
+/**
+ * Application index
+ *
+ * PHP version 5
+ *
+ * @category Public
+ * @package  Viewer
+ * @author   Johan Cwiklinski <johan.cwiklinski@anaphore.eu>
+ * @license  Unknown http://unknown.com
+ * @link     http://anaphore.eu
+ */
 //Main application entry for production
 define('APP_DIR', realpath(__DIR__  . '/../app'));
 define('WEB_DIR', realpath(__DIR__  . '/../web'));
