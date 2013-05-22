b884410215adf7d76bb4c5adc4b6bff649617cc7
Johan Cwiklinski <johan.cwiklinski@anaphore.eu>
2013-05-22 13:20:14 +0200

    Check PHP version, limit debug access per IP

--- web/debug.php
+++ web/debug.php
@@ -1,4 +1,31 @@
 <?php
+/**
+ * Application index for debugging purposes
+ *
+ * PHP version 5
+ *
+ * @category Public
+ * @package  Viewer
+ * @author   Johan Cwiklinski <johan.cwiklinski@anaphore.eu>
+ * @license  Unknown http://unknown.com
+ * @link     http://anaphore.eu
+ */
+
+if ( version_compare(PHP_VERSION, '5.3', '<') ) {
+    echo 'This application is NO compliant with your current PHP version.<br/>' .
+        'Application requires PHP 5.3 minimum, and you current version is ' .
+        PHP_VERSION;
+    die();
+}
+
+$dbg_ips = array(
+    '127.0.0.1'
+);
+
+if ( !in_array($_SERVER["REMOTE_ADDR"], $dbg_ips) ) {
+    die('Your are not allowed to debug this application.');
+}
+
 //Main application entry for debuging purposes
 define('APP_DIR', realpath(__DIR__  . '/../app'));
 define('WEB_DIR', realpath(__DIR__  . '/../web'));
--- web/index.php
+++ web/index.php
@@ -10,6 +10,14 @@
  * @license  Unknown http://unknown.com
  * @link     http://anaphore.eu
  */
+
+if ( version_compare(PHP_VERSION, '5.3', '<') ) {
+    echo 'This application is NO compliant with your current PHP version.<br/>' .
+        'Application requires PHP 5.3 minimum, and you current version is ' .
+        PHP_VERSION;
+    die();
+}
+
 //Main application entry for production
 define('APP_DIR', realpath(__DIR__  . '/../app'));
 define('WEB_DIR', realpath(__DIR__  . '/../web'));
