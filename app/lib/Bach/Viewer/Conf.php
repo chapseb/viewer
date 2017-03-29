b32601ec8bb5e6d2ca87a4e38b9a9fec36077903
Sebastien Chaptal <sebastien.chaptal@anaphore.eu>
2017-03-29 12:04:56 +0200

    Remove redis
    temporarily

--- app/lib/Bach/Viewer/Conf.php
+++ app/lib/Bach/Viewer/Conf.php
@@ -73,9 +73,6 @@ class Conf
     private $_prepared_path;
     private $_prepare_method;
     private $_known_methods;
-    private $_redis_addr;
-    private $_redis_port;
-    private $_redis_session;
     private $_debug_mode;
 
     private $_known_remote_methods = array(
@@ -179,9 +176,6 @@ class Conf
                 'uri'       => $this->_conf['remote_infos']['uri']
             );
         }
-        $this->_redis_addr            = $this->_conf['redis_addr'];
-        $this->_redis_port            = $this->_conf['redis_port'];
-        $this->_redis_session         = $this->_conf['redis_session'];
         $this->_debug_mode            = $this->_conf['debug_mode'];
     }
 
@@ -392,36 +386,6 @@ class Conf
         if ( defined('APP_TESTS') ) {
             $this->_setRoots($roots);
         }
-    }
-
-    /**
-     * Retrieve redis address in config
-     *
-     * @return string
-     */
-    public function getRedisAddr()
-    {
-        return $this->_redis_addr;
-    }
-
-    /**
-     * Retrieve redis port in config
-     *
-     * @return string
-     */
-    public function getRedisPort()
-    {
-        return $this->_redis_port;
-    }
-
-    /**
-     * Retrieve redis session name in config
-     *
-     * @return string
-     */
-    public function getRedisSession()
-    {
-        return $this->_redis_session;
     }
 
     /**
