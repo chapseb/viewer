dda2d1a5a15d97ed78c2a07c9a041ca154217414
Sebastien Chaptal <sebastien.chaptal@anaphore.eu>
2017-10-25 15:54:32 +0200

    Fix previous commit

--- web/scripts/bachview/iipnavigation.js
+++ web/scripts/bachview/iipnavigation.js
@@ -154,14 +154,14 @@ var BNavigation = new Class({
         });
         $$('#printZoomify').addEvent('click', function(){
             // Firefox 1.0+
-            var isFirefox = typeof InstallTrigger !== 'undefined';
+            //var isFirefox = typeof InstallTrigger !== 'undefined';
 
             // Safari 3.0+ "[object HTMLElementConstructor]" 
-            var isSafari = /constructor/i.test(window.HTMLElement) || (function (p) { return p.toString() === "[object SafariRemoteNotification]"; })(!window['safari'] || (typeof safari !== 'undefined' && safari.pushNotification));
+            //var isSafari = /constructor/i.test(window.HTMLElement) || (function (p) { return p.toString() === "[object SafariRemoteNotification]"; })(!window['safari'] || (typeof safari !== 'undefined' && safari.pushNotification));
 
-            if (isFirefox || isSafari) {
+            //if (isFirefox || isSafari) {
                 alert("Pensez à mettre l'orientation en mode paysage et l'échelle à 50% lors de l'impression");
-            }
+            //}
             window.print();
         });
     },
